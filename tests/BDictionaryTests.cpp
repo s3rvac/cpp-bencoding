ee3612be6bc984ebfe36dd5ed9cab269938d0a33
Petr Zemek <s3rvac@gmail.com>
2014-08-03 11:23:05 +0200

    Add a basic directory structure + CMakeLists.txt files.

---
+++ tests/BDictionaryTests.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+class BDictionaryTests: public Test {};
---
+++ tests/DecoderTests.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+class DecoderTests: public Test {};
