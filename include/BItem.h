40d66d218ceeb2d2a5bb8f929b8c9b7f5d0f2596
Petr Zemek <s3rvac@gmail.com>
2014-08-03 18:58:26 +0200

    BItem: Add a new member function template: as<>().

--- include/BItem.h
+++ include/BItem.h
@@ -8,14 +8,29 @@
 #ifndef BENCODING_BITEM_H
 #define BENCODING_BITEM_H
 
+#include <memory>
+
 namespace bencoding {
 
 /**
 * @brief Base class for all items (integers, strings, etc.).
 */
-class BItem {
+class BItem: public std::enable_shared_from_this<BItem> {
 public:
 	virtual ~BItem() = 0;
+
+	/**
+	* @brief Casts the item to the given subclass of BItem.
+	*
+	* @tparam T Subclass of BItem.
+	*/
+	template <typename T>
+	std::shared_ptr<T> as() {
+		static_assert(std::is_base_of<BItem, T>::value,
+			"T has to be a subclass of BItem");
+
+		return std::dynamic_pointer_cast<T>(shared_from_this());
+	}
 };
 
 } // namespace bencoding
