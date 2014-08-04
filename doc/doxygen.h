35aedef6490dae31cb00b09c826c18977c2a9128
Petr Zemek <s3rvac@gmail.com>
2014-08-04 18:54:45 +0200

    Add a documentation for the bencoding namespace.

--- doc/doxygen.h
+++ doc/doxygen.h
@@ -3,3 +3,6 @@
 *
 * This is an automatically generated API documentation for the @c cpp-bencoding project.
 */
+
+// Document the bencoding namespace (there is no better place).
+/// @namespace bencoding Main namespace of the bencoding library.
