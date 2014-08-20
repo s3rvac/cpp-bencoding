bd8a7dda13b3d11294003af34651d1213e46426e
Petr Zemek <s3rvac@gmail.com>
2014-08-20 14:49:43 +0200

    doc: Document the doxygen.h file itself.

--- doc/doxygen.h
+++ doc/doxygen.h
@@ -1,4 +1,11 @@
 /**
+* @file      doxygen.h
+* @copyright (c) 2014 by Petr Zemek (s3rvac@gmail.com) and contributors
+* @license   BSD, see the @c LICENSE file for more details
+* @brief     Documentation of the main page and the used namespace.
+*/
+
+/**
 * @mainpage
 *
 * This is an automatically generated API documentation for the @c cpp-bencoding project.
