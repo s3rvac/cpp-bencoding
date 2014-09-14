ef22acc375bc2036f727b30141b881edc041094b
Petr Zemek <s3rvac@gmail.com>
2014-09-14 09:51:05 +0200

    BDictionary: Fix the file description.

--- include/BDictionary.h
+++ include/BDictionary.h
@@ -2,7 +2,7 @@
 * @file      BDictionary.h
 * @copyright (c) 2014 by Petr Zemek (s3rvac@gmail.com) and contributors
 * @license   BSD, see the @c LICENSE file for more details
-* @brief     Representation of a list.
+* @brief     Representation of a dictionary.
 */
 
 #ifndef BENCODING_BDICTIONARY_H
